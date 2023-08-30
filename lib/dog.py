8be57174f05f37988b171f26006d71d5aa9ea25c
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-30 10:39:50 -0500

    init function

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='name', breed='Mastiff'):
+        if(type(name)==str and len(name)>1 and len(name)<25 and breed in APPROVED_BREEDS):
+            self.name = name
+            self.breed = breed
+        elif(type(name)==str and len(name)>1 and len(name)<25):
+            self.name = name
+            print("Breed must be in list of approved breeds.")
+        elif(breed in APPROVED_BREEDS):
+            self.breed = breed
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            print("Breed must be in list of approved breeds.")
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,12 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='name', job='Admin'):
+        if(type(name)==str and len(name)>0 and len(name)<26 and job in APPROVED_JOBS):
+            self.name = name.title()
+            self.job = job
+        elif(type(name)==str and len(name)>0 and len(name)<26):
+            self.name = name.title()
+            print("Job must be in list of approved jobs.")
+        else:
+            print("Name must be string between 1 and 25 characters.")
